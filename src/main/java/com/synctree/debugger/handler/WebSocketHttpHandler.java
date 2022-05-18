a5894748e9f3fe3e21c704d35aa2277b608042f1
Synctree R&D Tech <90591320+SynctreeRndTech@users.noreply.github.com>
2022-05-18 16:27:11 +0900

    20220518_Jianna: extra_id 없는 경우 에러처리 추가, json-simple 라이브러리로 변경
    1) json-simple 라이브러리 사용하도록 변경
    2) extra_id 없는 경우 에러처리 추가
    3) 에러메시지 방식 변경 -> message type으로 디버거로 return하여 프론트에 출력되게끔 처리

--- src/main/java/com/synctree/debugger/handler/WebSocketHttpHandler.java
+++ src/main/java/com/synctree/debugger/handler/WebSocketHttpHandler.java
@@ -3,7 +3,7 @@ package com.synctree.debugger.handler;
 import java.io.IOException;
 import java.util.HashMap;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -26,7 +26,7 @@ public class WebSocketHttpHandler extends TextWebSocketHandler {
 	//private static Set<WebSocketSession> sessions = new ConcurrentHashMap().newKeySet();
 	private static HashMap<String, WebSocketSession> sessions = new HashMap<String, WebSocketSession>();
 	private HashMap<String, String> lockKeyMap = new HashMap<String, String>();
-	
+	private HashMap<String, Object> hashmap = new HashMap<>();
 	//private final StringRedisTemplate stringRedisTemplate;
 	private final RedisUtil redisUtil;
 	
@@ -38,11 +38,11 @@ public class WebSocketHttpHandler extends TextWebSocketHandler {
         //logger.info("::: connected client ::: "+ session.getRemoteAddress().toString());
         //logger.info("::: handshake headers ::: "+ session.getHandshakeHeaders().toString());
         
-    	JSONObject jsonObj = new JSONObject();
-        jsonObj.put("msg_type", "text");
-        jsonObj.put("msg_data", "session_id");
-        jsonObj.put("id", session.getId());
+    	hashmap.put("msg_type", "text");
+    	hashmap.put("msg_data", "session_id");
+    	hashmap.put("id", session.getId());
         
+    	JSONObject jsonObj = new JSONObject(hashmap);
         session.sendMessage(new TextMessage(jsonObj.toString()));
     }
 
@@ -58,35 +58,38 @@ public class WebSocketHttpHandler extends TextWebSocketHandler {
     	
     	if(payloadSplit[0].equals("request_for_unlock")){
         	String lockKey = lockKeyMap.get(sessionID);
-    		jsonObj.put("msg_type", "text");
-    		jsonObj.put("msg_data", "unlock_result");
+        	hashmap.put("msg_type", "text");
+        	hashmap.put("msg_data", "unlock_result");
     		if (lockKey != null ) {
         		boolean result = redisUtil.setRedisStringValue(lockKey, "0"); //Release spin lock (unlock:"0", lock:"1")
         		if(result == true ) {
-                    jsonObj.put("result_msg", "spin_lock_released");
+        			hashmap.put("result_msg", "Spinlock released");
         		} else {
-        			jsonObj.put("result_msg", "release_spin_lock_failed");
+        			hashmap.put("result_msg", "Release spinlock failed");
         		}
+        		jsonObj.putAll(hashmap);
                 session.sendMessage(new TextMessage(jsonObj.toString()));
     		} else {
                 logger.info("[message handled] / session_id '" + sessionID + "''s lock_key is null");
-    			jsonObj.put("result_msg", "lock_key_is_null" );
+                hashmap.put("result_msg", "LockKey is null" );
     			session.sendMessage(new TextMessage(jsonObj.toString()));
     		}
     	} else if (payloadSplit[0].equals("GET_SESSION_ID")){
-    		jsonObj.put("msg_type", "text");
-            jsonObj.put("msg_data", "session_id");
-            jsonObj.put("id", session.getId());
+    		hashmap.put("msg_type", "text");
+    		hashmap.put("msg_data", "session_id");
+    		hashmap.put("id", session.getId());
             
+    		jsonObj.putAll(hashmap);
             session.sendMessage(new TextMessage(jsonObj.toString()));
             
     	} else if(payloadSplit[0].equals("request_for_close")){
     		session.close();
     	} else {
-    		jsonObj.put("msg_type", "text");
-            jsonObj.put("msg_data", "echo_message");
-            jsonObj.put("payload", message.getPayload());
+    		hashmap.put("msg_type", "text");
+    		hashmap.put("msg_data", "echo_message");
+    		hashmap.put("payload", message.getPayload());
             
+    		jsonObj.putAll(hashmap);
             session.sendMessage(new TextMessage(jsonObj.toString()));
     	}
 
@@ -99,29 +102,54 @@ public class WebSocketHttpHandler extends TextWebSocketHandler {
         sessions.remove(session.getId());
     }
     
-    public boolean sendMessageToSession(DebuggerVo debuggerVo) throws Exception {
-        if (sessions == null) {
-        	logger.info("[send_message_to_session failed] / session_id: " + debuggerVo.getSessionId() + ", message: sessionList is null");
-            return false; //To-Do: 에러 처리
-        }
-        
-        WebSocketSession singleSession = sessions.get(debuggerVo.getSessionId());
+    public String sendMessageToSession(DebuggerVo debuggerVo) throws Exception {
+    	
+    	WebSocketSession singleSession = null;
     	JSONObject jsonObj = new JSONObject();
     	
+        if (sessions == null) {
+        	logger.info("[send_message_to_session failed] / session_id: " + debuggerVo.getSessionId() + ", message: session_list is null");
+        	return "sessions_all_null";
+        }
+        
+        if(debuggerVo.getSessionId() != null) {
+            singleSession = sessions.get(debuggerVo.getSessionId());
+        } else {
+        	logger.info("[send_message_to_session failed] / session_id: null, message: session_id is null");
+        	return "session_id_null";
+        }
+        
         try {    	
         	
 	        if(singleSession != null) {
-	        	lockKeyMap.put(debuggerVo.getSessionId(), debuggerVo.getLockKey());
-	            String extraId = debuggerVo.getExtraId();
-	    		jsonObj.put("msg_type", "json");
-	            jsonObj.put("msg_data", "extra_id");
-	            jsonObj.put("id", extraId);
-	            
-	            singleSession.sendMessage(new TextMessage(jsonObj.toString()));
-	
+	        	if(debuggerVo.getLockKey() != null) {
+	        		if(debuggerVo.getExtraId() != null) {
+			        	lockKeyMap.put(debuggerVo.getSessionId(), debuggerVo.getLockKey());
+			        	hashmap.put("msg_type", "json");
+			        	hashmap.put("msg_data", "extra_id");
+			        	hashmap.put("id", debuggerVo.getExtraId());
+			            
+			        	jsonObj.putAll(hashmap);
+			            singleSession.sendMessage(new TextMessage(jsonObj.toString()));
+	        		} else {
+			        	lockKeyMap.put(debuggerVo.getSessionId(), debuggerVo.getLockKey());
+			        	hashmap.put("msg_type", "json");
+			        	hashmap.put("msg_data", "error_msg");
+			        	hashmap.put("msg", "ExtraId is null");
+			            
+			        	jsonObj.putAll(hashmap);
+			            singleSession.sendMessage(new TextMessage(jsonObj.toString()));
+			            
+	        			logger.info("[send_message_to_session failed] / session_id: " + debuggerVo.getSessionId() + ", message: extra_id is null");
+	        			return "ExtraId is null";
+	        		}
+	        	} else {
+		        	logger.info("[send_message_to_session failed] / session_id: " + debuggerVo.getSessionId() + ", message: lock_key is null");
+		        	return "LockKey is null";
+	        	}
 	        } else {
 	        	logger.info("[send_message_to_session failed] / session_id: " + debuggerVo.getSessionId() + ", message: single_session is null");
-	        	return false;
+	        	return "Failed to find session";
 	        }
 		} catch (IOException e) {
 			logger.error("[IOException] / session_id:" + debuggerVo.getSessionId() + ", message: " + e.getMessage());
@@ -131,6 +159,6 @@ public class WebSocketHttpHandler extends TextWebSocketHandler {
 			logger.error("[Exception] / session_id:" + debuggerVo.getSessionId() + ", cause: " + e.getCause());
 		}
         
-        return true;
+        return "Delivery Succeed";
     }
 }
